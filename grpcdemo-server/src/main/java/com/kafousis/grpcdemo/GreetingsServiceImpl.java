74f9f997ddced717e2742b4356482402712eecec
Giannis Kafousis <g.kafousis@gmail.com>
2026-06-26 19:08:51 +0300

    unary api - server impl

---
+++ grpcdemo-server/src/main/java/com/kafousis/grpcdemo/GreetingsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.kafousis.grpcdemo;
+
+import com.kafousis.grpcdemo.proto.greet.GreetRequest;
+import com.kafousis.grpcdemo.proto.greet.GreetResponse;
+import com.kafousis.grpcdemo.proto.greet.GreetServiceGrpc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.grpc.server.service.GrpcService;
+
+import io.grpc.stub.StreamObserver;
+
+@GrpcService
+public class GreetingsServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
+    public static final Logger log = LoggerFactory.getLogger(GreetingsServiceImpl.class);
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+        log.info("Received greet request for: {}", request.getGreeting().getFirstName());
+
+        String greeting = "Hello, " + request.getGreeting().getFirstName() + "!";
+
+        GreetResponse response = GreetResponse.newBuilder()
+                .setResult(greeting)
+                .build();
+
+        // send the response back to the client
+        responseObserver.onNext(response);
+
+        // complete the RPC call
+        responseObserver.onCompleted();
+    }
+}
