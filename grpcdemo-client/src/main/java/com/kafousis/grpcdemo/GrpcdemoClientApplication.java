94a54ec630005e98d2d7a8759fb346a034cc4013
Giannis Kafousis <g.kafousis@gmail.com>
2026-06-26 18:40:28 +0300

    gRPC server, client modules

---
+++ grpcdemo-client/src/main/java/com/kafousis/grpcdemo/GrpcdemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.kafousis.grpcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcdemoClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcdemoClientApplication.class, args);
+    }
+
+}
---
+++ grpcdemo-server/src/test/java/com/kafousis/grpcdemo/GrpcdemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafousis.grpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcdemoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
